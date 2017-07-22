100c515389bda7baf0ad7ddf7dede8d663d5fef5
ErrOrnAmE <do.not.press.enter@gmail.com>
2017-07-22 15:30:32 +0200

    Add timeline

--- README.md
+++ README.md
@@ -4,5 +4,7 @@
 
 * HTML5/CSS3
 * Bulma 0.4.2
+* Spectre experimental
+* Hint 2.5.0
 * Vue.js 2.4.0
 * Vue-router 2.7.0
--- libs/app.js
+++ libs/app.js
@@ -2,7 +2,7 @@
 // Templates
 
 const Intro = { template: '#intro' };
-const Education = { template: '#education' };
+const Career = { template: '#career', props: ['career','education'] };
 const Projects = { template: '#projects', props: ['projects']};
 const Project = { template: '#project' };
 const Contact = { template: '#contact' };
@@ -11,7 +11,7 @@ const Contact = { template: '#contact' };
 
 const routes = [
 	{ name: 'intro', path: '/', component: Intro },
-	{ name: 'education', path: '/education', component: Education },
+	{ name: 'career', path: '/career', component: Career },
 	{ name: 'projects', path: '/projects', component: Projects },
 	{ name: 'project', path: '/projects/:project_slug', component: Project },
 	{ name: 'contact', path: '/contact', component: Contact}
@@ -29,7 +29,51 @@ const router = new VueRouter({
 
 const app = new Vue({
 	data: {
-		projects: ['OER', 'QRManager']
+		projects: ['OER', 'QRManager'],
+		career: [
+			{
+				checked: false,
+				title: "Internship",
+				subtitle: "@Your company?",
+				text: "I'm looking for an internship as a Junior Software Architect",
+				from: "Jan 2018",
+				to: "June 2018"
+			},
+			{
+				checked: true,
+				title: "Software Architect",
+				subtitle: "@Neosmose",
+				text: "Réalisation de nombreux projets informatiques: site d'e-learning, gestion de données d'appareils connectés, intranet, site vitrine, etc.",
+				from: "July 2016",
+				to: "Dec 2017"
+			}
+		],
+		education: [
+			{
+				checked: false,
+				title: "Master's degree in Software architecture",
+				subtitle: "@University of Nantes",
+				text: "Some text, blabla blabla bla",
+				from: "Sept 2016",
+				to: "June 2018"
+			},
+			{
+				checked: true,
+				title: "Bachelor's degree in Computer Games Development",
+				subtitle: "@University of the West of Scotland",
+				text: "Some text, blabla blabla bla",
+				from: "Sept 2015",
+				to: "June 2016"
+			},
+			{
+				checked: true,
+				title: "DUT in Computer Engineering",
+				subtitle: "@IUT of Nantes",
+				text: "Some text, blabla blabla bla",
+				from: "Sept 2013",
+				to: "June 2015"
+			},
+		]
 	},
 	router: router
 }).$mount('#app');
